a3024c8785c6a67d8ff396232230e661ee294aa0
gorangg <gorangajsek22@gmail.com>
2023-06-05 13:57:37 +0200

    master

---
+++ src/Messages.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import React from 'react';
+
+class Messages extends Component {
+	render() {
+		const { messages } = this.props;
+		return (
+			<ul className='Messages-list'>
+				{messages.map((m) => this.renderMessage(m))}
+			</ul>
+		);
+	}
+
+	renderMessage(message) {
+		const { member, text, timestamp } = message;
+		const { currentMember } = this.props;
+		const messageFromMe = member.id === currentMember.id;
+		const className = messageFromMe
+			? 'Messages-message currentMember'
+			: 'Messages-message';
+
+		let messageTime = '';
+		const time = new Date(timestamp);
+		messageTime = time.toLocaleTimeString([], {
+			hour: '2-digit',
+			minute: '2-digit',
+		});
+
+		return (
+			<li className={className}>
+				<span
+					className='avatar'
+					style={{ backgroundColor: member.clientData.color }}
+				/>
+				<div className='Message-content'>
+					<div className='username'>{member.clientData.username}</div>
+					<div className='text'>
+						{text}
+						<span className='time'>{messageTime}</span>
+					</div>
+				</div>
+			</li>
+		);
+	}
+}
+
+export default Messages;
